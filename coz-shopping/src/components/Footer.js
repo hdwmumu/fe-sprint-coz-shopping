d99ec04880154a203a47001ec2089415a26bf69b
hdwmumu <keumhe0110@gmail.com>
2023-05-15 15:59:20 +0900

    1차 리뷰

--- coz-shopping/src/components/Footer.js
+++ coz-shopping/src/components/Footer.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      <p>
+        개인정보 처리방침 | 이용 약관
+        <br />
+        All rights reserved @ Codestates
+      </p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- coz-shopping/src/components/Header.js
+++ coz-shopping/src/components/Header.js
@@ -1,21 +1,15 @@
 import React from "react";
 import styled from "styled-components";
 
-
-
-
-function Header(){
+function Header() {
   return (
-    <nav class="navber">
-      <div className="navber_logo">
-    <img src="img/로고.png" alt="logo" />
+    <nav>
+      <div>
+        <img src="img/로고.png" alt="logo" />
       </div>
-      <div className="navbar_menu">
-
-      </div>
-
+      <div></div>
     </nav>
-  )
+  );
 }
 // const Header = () => {
 //   return (
