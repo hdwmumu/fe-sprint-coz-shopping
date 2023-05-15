3f6e84d6e9fec12186e9f84792674749045ad6f0
hdwmumu <keumhe0110@gmail.com>
2023-05-15 20:38:58 +0900

    🐣 Footer 하단 고정

--- coz-shopping/src/App.js
+++ coz-shopping/src/App.js
@@ -1,9 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import styled from "styled-components"
 
-import HomeMain from "./Page/HomeMain";
-import ProductList from "./Page/ProductList";
+import HomeMain from "./components/HomeMain/HomeMain";
+import ProductList from "./components/ProductList/ProductList";
 import BookMark from "./components/BookMark/BookMark";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
--- coz-shopping/src/components/Footer.js
+++ coz-shopping/src/components/Footer.js
@@ -1,14 +1,34 @@
-import React from "react";
+import styled from "styled-components";
 
+const FooterWrapper = styled.footer`
+box-sizing: border-box;
+width: 100%;
+height: 58px;
+background: #FFFFFF;
+border-top: 1px solid rgba(0, 0, 0, 0.1);
+position: absolute;
+bottom: 0;
+`;
+
+
+const Content = styled.p`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  color: #888888 ;
+  line-height: 88.02%;
+  /* or 11px */
+  display: flex;
+  justify-content: center;
+  
+`;
 const Footer = () => {
   return (
-    <div>
-      <p>
-        개인정보 처리방침 | 이용 약관
-        <br />
-        All rights reserved @ Codestates
-      </p>
-    </div>
+    <FooterWrapper>
+      <Content>개인정보 처리방침 | 이용 약관</Content>
+      <Content>All rights reserved @ Codestates</Content>
+    </FooterWrapper>
   );
 };
 
