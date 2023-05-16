6d9a23af8a976fa2c0a6c8337d9a808c9b8e58d3
hdwmumu <keumhe0110@gmail.com>
2023-05-16 12:05:32 +0900

    헤더 디자인 완료

--- coz-shopping/src/components/Header.js
+++ coz-shopping/src/components/Header.js
@@ -1,54 +1,57 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import logo from "../assets/로고.png"
-import coz from "../assets/쇼핑몰 이름.png"
-import hamburger from "../assets/아이콘.png"
+import logo from "../assets/로고.png";
+import coz from "../assets/쇼핑몰 이름.png";
+import hamburger from "../assets/아이콘.png";
 import styled from "styled-components";
 
-const HeaderWrapp =styled.header`
-position: fixed;
-top: 0;
+const HeaderWrapp = styled.header`
+  display: flex;
+  position: fixed;
+  top: 0;
 
-box-sizing: border-box;
-border-top: 1px solid rgba(0, 0, 0, 0.1);
-box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.1);
-background-color: white;
-width: 100%;
-height: 80px;
+  box-sizing: border-box;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.1);
+  background-color: white;
+  width: 100%;
+  height: 80px;
 `;
-const CodeStatesLogo =styled.img`
-
+const CodeStatesLogo = styled.img`
+  margin: 23px 0px 0px 70px;
+  height: 33px;
 `;
 
 const Cozshopping = styled.img`
-
+  margin-left: 12px;
 `;
 
 const Hamburgerbar = styled.img`
-justify-content: flex-end;
+  margin-left: auto;
+  height: 30px;
+  margin-top : 20px;
+  margin-right: 78px;
+
 `;
 
 function Header() {
-  const [ isModalOpen, setIsModalOpen ] =useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleModal=()=> {
-    setIsModalOpen((prev)=> !prev)
-  }
+  const handleModal = () => {
+    setIsModalOpen((prev) => !prev);
+  };
 
   return (
-<>
-     <HeaderWrapp>
-      <Link to="/">
-        <CodeStatesLogo src={logo}></CodeStatesLogo>
-       <Cozshopping src={coz}></Cozshopping>
-      </Link>
-      <Hamburgerbar src={hamburger} onClick={handleModal}></Hamburgerbar>
-      
-    </HeaderWrapp>
-   
+    <>
+      <HeaderWrapp>
+        <Link to="/">
+          <CodeStatesLogo src={logo}></CodeStatesLogo>
+          <Cozshopping src={coz}></Cozshopping>
+        </Link>
+        <Hamburgerbar src={hamburger} onClick={handleModal}></Hamburgerbar>
+      </HeaderWrapp>
     </>
   );
 }
-
 
 export default Header;
